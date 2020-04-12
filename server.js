181c49af1430a60cdae9d5e9300a758fb2ef690a
Seun Martins <seundotme@gmail.com>
2020-04-12 16:24:41 +0200

    Restructure server

--- server.js
+++ server.js
@@ -1,17 +1,19 @@
-import express, { query } from "express";
+import express from "express";
 import morganLogger from "morgan";
 import querystring from "querystring";
 import axios from "axios";
 import cookieParser from "cookie-parser";
+import { generateRandomString } from "./utils";
 
-// setup envalid
 
-const app = express();
+// TODO: Setup envalid
 const port = process.env.PORT || 1965;
 const client_id = process.env.CLIENT_ID;
 const client_secret = process.env.CLIENT_SECRET;
 const redirect_uri = process.env.REDIRECT_URI;
-var stateKey = "spotify_auth_state";
+const stateKey = process.env.SPOTIFY_STATE_KEY || "spotify_auth_state";
+
+const app = express();
 
 app
   .use(express.static(`${__dirname}/public`))
@@ -56,45 +58,27 @@ app.get("/callback", async (req, res) => {
       grant_type: "authorization_code",
     }),
     headers: {
-      Authorization: `Basic ${new Buffer(
-        client_id + ":" + client_secret
+      Authorization: `Basic ${Buffer.from(
+        `${client_id}:${client_secret}`
       ).toString("base64")}`,
     },
   };
 
   try {
-    var tokenAPIResponse = await axios(options);
+    var { data } = await axios(options);
   } catch (e) {
     console.log(e);
     return res.redirect(
-      "/#" +
-        querystring.stringify({
-          error: "invalid_token",
-        })
+      `/#${querystring.stringify({
+        error: "invalid_token",
+      })}`
     );
   }
-  // data: {
-  //   access_token: 'BQCT_lObJGqToRm2CCa5AZs6XhqZGovGWT4HdbN4LA0qCiaPzM2l_7SLXocOwrpleYEpd8YnAp9-jERPzzz_wR5EE0bMedvTgBthkW5w7uw1f78DNbLqSChTYFzST26Xbofqcw8xqgE5JDEiKyAaoEGqT7f5w5n7JK7S7hGM1py2gYV-aw',
-  //   token_type: 'Bearer',
-  //   expires_in: 3600,
-  //   refresh_token: 'AQCF1SLrX1VBxqmfeE75jxDtWWqh-VhylVnnX2i84Mx9X7ie8w8N8qxqOXHo9fovtYukI4pfPrqxqY72ocPkGgV8_SNNbNdgCowPF6VmiVekxZWl-dum8pDHPMJzawadWxc',
-  //   scope: 'user-read-email user-read-private'
-  // }
-  console.log(tokenAPIResponse);
+
+  res.redirect(
+    `/#${querystring.stringify({
+      access_token: data.access_token,
+      refresh_token: data.refresh_token,
+    })}`
+  );
 });
-
-/**
- * Generates a random string containing numbers and letters
- * @param  {number} length The length of the string
- * @return {string} The generated string
- */
-function generateRandomString(length) {
-  var text = "";
-  var possible =
-    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-
-  for (var i = 0; i < length; i++) {
-    text += possible.charAt(Math.floor(Math.random() * possible.length));
-  }
-  return text;
-}
