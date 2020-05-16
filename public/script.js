7668fd9131d7bd67d07e749cb958a04388edf40e
Seun Martins <seundotme@gmail.com>
2020-05-17 01:40:46 +0200

    Added commit hook for Prettier

--- public/script.js
+++ public/script.js
@@ -1,13 +1,15 @@
-(function () {
+(function init(window) {
+  const { document, alert, $, Handlebars } = window;
   /**
    * Obtains parameters from the hash of the URL
    * @return Object
    */
   function getHashParams() {
-    var hashParams = {};
-    var e, r = /([^&;=]+)=?([^&;]*)/g,
+    let hashParams = {},
+      e,
+      r = /([^&;=]+)=?([^&;]*)/g,
       q = window.location.hash.substring(1);
-    while (e = r.exec(q)) {
+    while ((e = r.exec(q))) {
       hashParams[e[1]] = decodeURIComponent(e[2]);
     }
     return hashParams;
@@ -16,33 +18,51 @@
   function handleButtonClick(e) {
     if (e.target.nodeName === "BUTTON") {
       e.stopPropagation();
-      var track = window.playlist.items[e.target.id].track;
+      const { track } = window.playlist.items[e.target.id];
       console.log("track", track);
       console.log("handle click", e);
       $.ajax({
         url: "https://api.deezer.com/search",
         data: {
-          q: `artist:\"${track.artists[0].name}\" track:\"${track.name}\"`
+          q: `artist:"${track.artists[0].name}"track:"${track.name}"`,
         },
-        success: function (response) {
+        success(response) {
           console.log(response);
-        }
+        },
       });
     }
+  }
+
+  var playlistSource = document.getElementById("playlist-template").innerHTML,
+    playlistTemplate = Handlebars.compile(playlistSource),
+    playlistPlaceholder = document.getElementById("playlists"),
+    params = getHashParams(),
+    { access_token } = params,
+    { error } = params;
+
+  if (error) {
+    alert("There was an error during the authentication");
+  } else if (access_token) {
+    $("#login").hide();
+    $("#loggedin").show();
+  } else {
+    // Render initial screen
+    $("#login").show();
+    $("#loggedin").hide();
   }
 
   function getSpotifyPlaylist(e) {
     console.log(e);
     e.stopPropagation();
 
-    var playlistUrl = $("input[type=text]").val();
+    const playlistUrl = $("input[type=text]").val();
 
     if (!playlistUrl) {
       return;
     }
 
-    var url = new URL(playlistUrl);
-    var playlistId = url.pathname.split("playlist/")[1]
+    const url = new URL(playlistUrl),
+      playlistId = url.pathname.split("playlist/")[1];
 
     if (!playlistId) {
       return;
@@ -50,46 +70,23 @@
 
     $.ajax({
       url: `https://api.spotify.com/v1/playlists/${playlistId}/tracks`,
-      headers: { 'Authorization': 'Bearer ' + access_token },
-      success: function (response) {
+      headers: { Authorization: `Bearer ${access_token}` },
+      success(response) {
         window.playlist = response;
         playlistPlaceholder.innerHTML = playlistTemplate(response);
-        $('#login').hide();
-        $('#loggedin').show();
+        $("#login").hide();
+        $("#loggedin").show();
 
-        var trackList = document.getElementById("track-list");
+        const trackList = document.getElementById("track-list");
         trackList.addEventListener("click", handleButtonClick);
-      }
+      },
     });
   }
 
-  var playlistSource = document.getElementById('playlist-template').innerHTML,
-    playlistTemplate = Handlebars.compile(playlistSource),
-    playlistPlaceholder = document.getElementById('playlists');
+  window.addEventListener("DOMContentLoaded", () => {
+    console.log("DOM fully loaded and parsed");
 
-  var params = getHashParams();
-
-  var access_token = params.access_token,
-    refresh_token = params.refresh_token,
-    error = params.error;
-
-  if (error) {
-    alert('There was an error during the authentication');
-  } else {
-    if (access_token) {
-      $('#login').hide();
-      $('#loggedin').show();
-    } else {
-      // render initial screen
-      $('#login').show();
-      $('#loggedin').hide();
-    }
-  }
-
-  window.addEventListener('DOMContentLoaded', (event) => {
-    console.log('DOM fully loaded and parsed');
-
-    var playlistForm = document.getElementById("playlist-form");
+    const playlistForm = document.getElementById("playlist-form");
     playlistForm.addEventListener("submit", getSpotifyPlaylist);
   });
-})();
+})(this);
